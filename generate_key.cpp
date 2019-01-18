5ae0aa7ad6ec01421abf9dfb826dcafa34d6b403
Martijn Otto <git@martijnotto.nl>
2019-01-18 15:24:41 +0100

    Create an extra subkey, so we don't have to keep the master key available

--- generate_key.cpp
+++ generate_key.cpp
@@ -29,12 +29,15 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 
     // derive the keys from the master
     derived_key<secret_key_size>    main_key_derivation             { master, 1, context };
-    derived_key<secret_key_size>    encryption_key_derivation       { master, 2, context };
-    derived_key<secret_key_size>    authentication_key_derivation   { master, 3, context };
+    derived_key<secret_key_size>    signing_key_derivation          { master, 2, context };
+    derived_key<secret_key_size>    encryption_key_derivation       { master, 3, context };
+    derived_key<secret_key_size>    authentication_key_derivation   { master, 4, context };
 
     // holders for the key data - public keys get an extra byte because of the leading 0x40 byte that we need to add for pgp to work
     std::vector<uint8_t>            main_key_public             (crypto_sign_PUBLICKEYBYTES + 1);           // main key, for signing and certification
     std::vector<uint8_t>            main_key_secret             (crypto_sign_SECRETKEYBYTES);               // using ed25519 curve
+    std::vector<uint8_t>            signing_key_public          (crypto_sign_PUBLICKEYBYTES + 1);           // signing and certification subkey
+    std::vector<uint8_t>            signing_key_secret          (crypto_sign_SECRETKEYBYTES);               // using ed25519 curve
     std::vector<uint8_t>            encryption_key_public       (crypto_scalarmult_curve25519_BYTES + 1);   // the subkey used for encryption
     std::vector<uint8_t>            encryption_key_secret       (crypto_scalarmult_curve25519_BYTES);       // using curve25519
     std::vector<uint8_t>            authentication_key_public   (crypto_sign_PUBLICKEYBYTES + 1);           // the subkey used for authentication
@@ -46,6 +49,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 
     // create the curve from the derived key
     crypto_sign_seed_keypair(main_key_public.data() + 1,            main_key_secret.data(),             main_key_derivation.data());
+    crypto_sign_seed_keypair(signing_key_public.data() + 1,         signing_key_secret.data(),          signing_key_derivation.data());
     crypto_sign_seed_keypair(temp_key_public.data(),                temp_key_secret.data(),             encryption_key_derivation.data());
     crypto_sign_seed_keypair(authentication_key_public.data() + 1,  authentication_key_secret.data(),   authentication_key_derivation.data());
 
@@ -55,6 +59,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 
     // throw away the public-key data from the secret key - pgp doesn't like it
     main_key_secret.resize(main_key_secret.size()                       - crypto_sign_PUBLICKEYBYTES);
+    signing_key_secret.resize(signing_key_secret.size()                 - crypto_sign_PUBLICKEYBYTES);
     authentication_key_secret.resize(authentication_key_secret.size()   - crypto_sign_PUBLICKEYBYTES);
 
     // reverse the curve25519 secret, since pgp stores this in little-endian format
@@ -62,6 +67,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 
     // set the silly public key leading byte
     main_key_public[0]          = 0x40;
+    signing_key_public[0]       = 0x40;
     encryption_key_public[0]    = 0x40;
     authentication_key_public[0]= 0x40;
 
@@ -69,7 +75,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
     std::vector<pgp::packet> packets;
 
     // allocate space for all the packets
-    packets.reserve(6);
+    packets.reserve(8);
 
     // add the primary key packet
     packets.emplace_back(
@@ -112,6 +118,40 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
         }}
     );
 
+    // add the signing subkey
+    packets.emplace_back(
+        mpark::in_place_type_t<pgp::secret_subkey>{},                           // we are building a secret subkey
+        creation,                                                               // created at
+        pgp::key_algorithm::eddsa,                                              // using the eddsa key algorithm
+        mpark::in_place_type_t<pgp::secret_key::eddsa_key_t>{},                 // key type
+        std::forward_as_tuple(                                                  // public arguments
+            pgp::curve_oid::ed25519(),                                          // curve to use
+            pgp::multiprecision_integer{ std::move(signing_key_public) }        // move in the public key point
+        ),
+        std::forward_as_tuple(                                                  // secret arguments
+            pgp::multiprecision_integer{ std::move(signing_key_secret) }        // move in the secret key point
+        )
+    );
+
+    // retrieve the newly created subkey
+    auto &signing_key = mpark::get<pgp::secret_subkey>(packets.back().body());
+
+    // now add a self-signature
+    packets.emplace_back(
+        mpark::in_place_type_t<pgp::signature>{},                               // subkey signature
+        mpark::in_place_type_t<pgp::eddsa_signature>{},                         // using eddsa signature generation
+        main_key,                                                               // we sign with the main key
+        signing_key,                                                            // indicating we own this subkey
+        pgp::signature_subpacket_set{{                                          // hashed subpackets
+            pgp::signature_creation_time_subpacket  { signature  },             // signature created at
+            pgp::key_expiration_time_subpacket      { expiration },             // signature expires at
+            pgp::key_flags_subpacket                { 0x01, 0x02 }              // used for certification and signing
+        }},
+        pgp::signature_subpacket_set{{                                          // unhashed subpackets
+            pgp::issuer_subpacket{ main_key.fingerprint() }                     // fingerprint of the signing key
+        }}
+    );
+
     // add the subkey for encryption
     packets.emplace_back(
         mpark::in_place_type_t<pgp::secret_subkey>{},                           // we are building a secret subkey
@@ -130,7 +170,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
     );
 
     // retrieve the newly created subkey
-    auto &encryption_key = mpark::get<pgp::secret_subkey>(packets[3].body());
+    auto &encryption_key = mpark::get<pgp::secret_subkey>(packets.back().body());
 
     // now add a self-signature
     packets.emplace_back(
@@ -164,7 +204,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
     );
 
     // retrieve the new authentication subkey
-    auto &authentication_key = mpark::get<pgp::secret_subkey>(packets[5].body());
+    auto &authentication_key = mpark::get<pgp::secret_subkey>(packets.back().body());
 
     // and add a signature for that as well
     packets.emplace_back(
