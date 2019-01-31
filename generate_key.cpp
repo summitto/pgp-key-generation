2d58658484b10ee273bbae9396a491e64c540519
rockhouse <rockhouse@users.noreply.github.com>
2019-01-31 20:41:50 +0100

    Working ECDSA key gen but code needs cleanup

--- generate_key.cpp
+++ generate_key.cpp
@@ -1,3 +1,11 @@
+/*TODO
+- no namespace CryptoPP
+- template Identity hash seperate
+- consts for key_flags_subpacket and replace hardcoded hex flags in key gen
+
+*/
+	
+
 #include "generate_key.h"
 #include "derived_key.h"
 #include "errors.h"
@@ -7,7 +15,56 @@
 #include <cryptopp/oids.h>
 #include <cryptopp/osrng.h>
 
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/secblock.h>
+
+
 using namespace CryptoPP;
+
+template <unsigned int HASH_SIZE = 32>
+class IdentityHash : public HashTransformation
+{
+public:
+    constexpr const static auto DIGESTSIZE = HASH_SIZE;
+
+    static const char * StaticAlgorithmName()
+    {
+        return "IdentityHash";
+    }
+
+    IdentityHash() : m_digest(HASH_SIZE), m_idx(0) {}
+
+    virtual unsigned int DigestSize() const
+    {
+        return DIGESTSIZE;
+    }
+
+    virtual void Update(const byte *input, size_t length)
+    {
+        size_t s = STDMIN(STDMIN<size_t>(DIGESTSIZE, length),
+                                         DIGESTSIZE - m_idx);    
+        if (s)
+            ::memcpy(&m_digest[m_idx], input, s);
+        m_idx += s;
+    }
+
+    virtual void TruncatedFinal(byte *digest, size_t digestSize)
+    {
+        ThrowIfInvalidTruncatedSize(digestSize);
+
+        if (m_idx != DIGESTSIZE)
+            throw Exception(Exception::OTHER_ERROR, "Input size must be " + IntToString(DIGESTSIZE));
+
+        if (digest)
+            ::memcpy(digest, m_digest, digestSize);
+
+        m_idx = 0;
+    }
+
+private:
+    SecByteBlock m_digest;
+    size_t m_idx;
+};
 
 /**
  *  Generate a complete key, including the required signatures
@@ -54,37 +111,24 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 
 	AutoSeededRandomPool prng;
 
-	ECDSA<ECP, SHA1>::PrivateKey k1;
+	ECDSA<ECP, IdentityHash<32>>::PrivateKey k1;
 	k1.Initialize( prng, ASN1::secp256r1() );
 
 	const Integer& x1 = k1.GetPrivateExponent();
 	std::cout << "K1: " << std::hex << x1 << std::dec << std::endl;
 
-	ECDSA<ECP, SHA1>::PrivateKey mainKeySecret;
+	ECDSA<ECP, IdentityHash<32>>::PrivateKey mainKeySecret;
 	CryptoPP::Integer mainKeySecret_x;
 	mainKeySecret_x.Decode(main_key_derivation.data(), SECRETKEYBYTES);
 
 	mainKeySecret.Initialize(ASN1::secp256r1(), mainKeySecret_x);
-	
-	// std::cout << "master: ";
-	// for(const auto& s: master)
-	//         std::cout << (unsigned int) s << ' ';
-	// std::cout << "\n" << std::endl;
-	//
-	// std::cout << "main_key_derivation: ";
-	// for(const auto& b: main_key_derivation)
-	//         std::cout << (unsigned int) b << ' ';
-	// std::cout << "\n" << std::endl;
-	//
-	// std::cout << "Integer mainKeySecret_x: " <<  mainKeySecret_x << std::endl;
-	// std::cout << "Integer mainKeySecret_x: " << std::hex << mainKeySecret_x << std::dec << std::endl;
-	
+		
 	const Integer& mainKeySecret_exponent = mainKeySecret.GetPrivateExponent();
 	mainKeySecret_exponent.Encode(main_key_secret.data(), main_key_secret.size());
 	
 	std::cout << "mainKeySecret_exponent: " <<  mainKeySecret_exponent << std::endl;
 	
-	ECDSA<ECP, SHA1>::PublicKey mainKeyPublic;
+	ECDSA<ECP, IdentityHash<32>>::PublicKey mainKeyPublic;
 	mainKeySecret.MakePublicKey(mainKeyPublic);
 	
 	const ECP::Point& mainKeyPublic_q = mainKeyPublic.GetPublicElement();
@@ -95,18 +139,16 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 
 
 
-
-
-	ECDSA<ECP, SHA1>::PrivateKey signingKeySecret;
+	ECDSA<ECP, IdentityHash<32>>::PrivateKey signingKeySecret;
 	CryptoPP::Integer signingKeySecret_x;
-	signingKeySecret_x.Decode(signing_key_derivation.data(), PUBLICKEYBYTES);
+	signingKeySecret_x.Decode(signing_key_derivation.data(), SECRETKEYBYTES);
 
 	signingKeySecret.Initialize(ASN1::secp256r1(), signingKeySecret_x);
 	
 	const Integer& signingKeySecret_exponent = signingKeySecret.GetPrivateExponent();
 	signingKeySecret_exponent.Encode(signing_key_secret.data(), signing_key_secret.size());
 	
-	ECDSA<ECP, SHA1>::PublicKey signingKeyPublic;
+	ECDSA<ECP, IdentityHash<32>>::PublicKey signingKeyPublic;
 	signingKeySecret.MakePublicKey(signingKeyPublic);
 	
 	const ECP::Point& signingKeyPublic_q = signingKeyPublic.GetPublicElement();
@@ -115,16 +157,16 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 
 
 
-	ECDSA<ECP, SHA1>::PrivateKey encryptionKeySecret;
+	ECDSA<ECP, IdentityHash<32>>::PrivateKey encryptionKeySecret;
 	CryptoPP::Integer encryptionKeySecret_x;
-	encryptionKeySecret_x.Decode(encryption_key_derivation.data(), PUBLICKEYBYTES);
+	encryptionKeySecret_x.Decode(encryption_key_derivation.data(), SECRETKEYBYTES);
 
 	encryptionKeySecret.Initialize(ASN1::secp256r1(), encryptionKeySecret_x);
 	
 	const Integer& encryptionKeySecret_exponent = encryptionKeySecret.GetPrivateExponent();
 	encryptionKeySecret_exponent.Encode(encryption_key_secret.data(), encryption_key_secret.size());
 	
-	ECDSA<ECP, SHA1>::PublicKey encryptionKeyPublic;
+	ECDSA<ECP, IdentityHash<32>>::PublicKey encryptionKeyPublic;
 	encryptionKeySecret.MakePublicKey(encryptionKeyPublic);
 	
 	const ECP::Point& encryptionKeyPublic_q = encryptionKeyPublic.GetPublicElement();
@@ -133,22 +175,21 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
 	
 	
 
-	ECDSA<ECP, SHA1>::PrivateKey authenticationKeySecret;
+	ECDSA<ECP, IdentityHash<32>>::PrivateKey authenticationKeySecret;
 	CryptoPP::Integer authenticationKeySecret_x;
-	authenticationKeySecret_x.Decode(authentication_key_derivation.data(), PUBLICKEYBYTES);
+	authenticationKeySecret_x.Decode(authentication_key_derivation.data(), SECRETKEYBYTES);
 
 	authenticationKeySecret.Initialize(ASN1::secp256r1(), authenticationKeySecret_x);
 	
 	const Integer& authenticationKeySecret_exponent = authenticationKeySecret.GetPrivateExponent();
 	authenticationKeySecret_exponent.Encode(authentication_key_secret.data(), authentication_key_secret.size());
 	
-	ECDSA<ECP, SHA1>::PublicKey authenticationKeyPublic;
+	ECDSA<ECP, IdentityHash<32>>::PublicKey authenticationKeyPublic;
 	authenticationKeySecret.MakePublicKey(authenticationKeyPublic);
 	
 	const ECP::Point& authenticationKeyPublic_q = authenticationKeyPublic.GetPublicElement();
 	authenticationKeyPublic_q.x.Encode(authentication_key_public.data() + 1, 32);
 	authenticationKeyPublic_q.y.Encode(authentication_key_public.data() + 1 + authenticationKeyPublic_q.x.MinEncodedSize(), 32);
-
 
 
     // set the silly public key leading byte
@@ -229,7 +270,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
         pgp::signature_subpacket_set{{                                          // hashed subpackets
             pgp::signature_creation_time_subpacket  { signature  },             // signature created at
             pgp::key_expiration_time_subpacket      { expiration },             // signature expires at
-            pgp::key_flags_subpacket                { 0x01, 0x02 }              // used for certification and signing
+            pgp::key_flags_subpacket                { 0x02 }              		// used for certification and signing
         }},
         pgp::signature_subpacket_set{{                                          // unhashed subpackets
             pgp::issuer_subpacket{ main_key.fingerprint() }                     // fingerprint of the signing key
@@ -297,7 +338,7 @@ std::vector<pgp::packet> generate_key(const master_key &master, std::string user
         pgp::signature_subpacket_set{{                                          // hashed subpackets
             pgp::signature_creation_time_subpacket  { signature  },             // signature created at
             pgp::key_expiration_time_subpacket      { expiration },             // signature expires at
-            pgp::key_flags_subpacket                { 0x20       }              // used for encryption of communications and storage
+            pgp::key_flags_subpacket                { 0x20       }              // used for encryption of communications and storage 
         }},
         pgp::signature_subpacket_set{{                                          // unhashed subpackets
             pgp::issuer_subpacket{ main_key.fingerprint() }                     // fingerprint of the signing key
