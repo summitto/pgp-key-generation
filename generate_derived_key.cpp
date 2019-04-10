ad06ebfa623628b02cac84d0ab565e83550b3a7a
Tom Smeding <tom.smeding@gmail.com>
2019-04-10 17:12:22 +0200

    Fix indentation in generate_derived_key.cpp

--- generate_derived_key.cpp
+++ generate_derived_key.cpp
@@ -98,10 +98,9 @@ int main(int argc, const char **argv)
                 dice_numbers.push_back(roll);
             }
         }
-    
-      // hash dice numbers together with random key
-      crypto_generichash(master.data(), master.size(), reinterpret_cast<const unsigned char *>( dice_numbers.data() ), dice_numbers.size(), master.data(), master.size()); 
-      
+
+        // hash dice numbers together with random key
+        crypto_generichash(master.data(), master.size(), reinterpret_cast<const unsigned char *>( dice_numbers.data() ), dice_numbers.size(), master.data(), master.size());
     }
 
     // convert the dates to a timestamp
