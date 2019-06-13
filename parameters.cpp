0370a423611ebfc1546521c55f465a6989954294
Tom Smeding <tom.smeding@gmail.com>
2019-06-13 14:06:27 +0200

    Use key_flag enum

--- parameters.cpp
+++ parameters.cpp
@@ -8,19 +8,19 @@ namespace parameters {
         switch (type) {
             case key_type::main:
                 // The main key is used for certification only.
-                return pgp::signature_subpacket::key_flags{ 0x01 };
+                return pgp::signature_subpacket::key_flags{ pgp::key_flag::certification };
 
             case key_type::signing:
                 // A signing key is used for certification and signing.
-                return pgp::signature_subpacket::key_flags{ 0x01, 0x02 };
+                return pgp::signature_subpacket::key_flags{ pgp::key_flag::certification, pgp::key_flag::signing };
 
             case key_type::encryption:
                 // An encryption key is used for communications and storage.
-                return pgp::signature_subpacket::key_flags{ 0x04, 0x08 };
+                return pgp::signature_subpacket::key_flags{ pgp::key_flag::encryption_communications, pgp::key_flag::encryption_storage };
 
             case key_type::authentication:
                 // An authentication key is used for authentication.
-                return pgp::signature_subpacket::key_flags{ 0x20 };
+                return pgp::signature_subpacket::key_flags{ pgp::key_flag::authentication };
         }
     }
 
