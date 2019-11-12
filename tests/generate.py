8c0755440b9aaaa5bf228dee4cb959544e64676d
Bert Peters <bert@bertptrs.nl>
2019-11-12 15:31:09 +0100

    Make data generation more pythonic.

--- tests/generate.py
+++ tests/generate.py
@@ -1,11 +1,12 @@
 import random
+import datetime
 
 import time
 from date_utils import *
 
 
-def generateString(**kwargs):
-    length = random.randint(kwargs.get("minstrlen", 1), kwargs.get("maxstrlen", 200))
+def generateString(minstrlen=11, maxstrlen=200):
+    length = random.randint(minstrlen, maxstrlen)
     return "".join([chr(random.randint(ord(' '), ord('~'))) for _ in range(length)])
 
 def generateName():
@@ -14,17 +15,13 @@ def generateName():
 def generateEmail():
     return generateString()
 
-def generateDate(**kwargs):
-    while True:
-        year = random.randint(1990, 2100)
-        month = random.randint(1, 12)
-        day = random.randint(1, days_in_month(year, month))
-        hour = random.randint(0, 23)
-        minute = random.randint(0, 59)
-        second = random.randint(0, 59)
-        string = "{:04}-{:02}-{:02} {:02}:{:02}:{:02}".format(year, month, day, hour, minute, second)
-        if kwargs.get("mindate_unix", 0) <= date_to_unix(string) <= kwargs.get("maxdate_unix", 2 ** 63):
-            return string
+def generateDate(mindate_unix=631152000, maxdate_unix=2**32):
+    # Python's date functions don't handle dates past 9999 properly,
+    # but this is a sensible default range
+    timestamp = random.randint(mindate_unix, maxdate_unix)
+    date = datetime.datetime.fromtimestamp(timestamp)
+
+    return date.isoformat(sep=' ')
 
 def generateDie():
     return random.randint(1, 6)
@@ -39,9 +36,9 @@ def generateSymmetricKey():
 def generateInput():
     # generate some dates in the right range
     now = int(time.time())
-    date_key_creation = generateDate(maxdate_unix = now)
-    date_creation = generateDate(maxdate_unix = now)
-    date_expiration = generateDate(mindate_unix = now)
+    date_key_creation = generateDate(maxdate_unix=now)
+    date_creation = generateDate(maxdate_unix=now)
+    date_expiration = generateDate(mindate_unix=now)
     # the key creation date has to be before the signature creation date
     date_key_creation, date_creation = sorted([date_key_creation, date_creation])
 
@@ -52,6 +49,6 @@ def generateInput():
         "expiration": date_expiration,
         "dice": generateDice(),
         "key": generateSymmetricKey(),
-        "context": generateString(minstrlen = 8, maxstrlen = 8),
+        "context": generateString(minstrlen=8, maxstrlen=8),
         "key_creation": date_key_creation
     }
