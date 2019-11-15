47e9da36d138cff44f6b49909abb9cdac249aa6d
Martijn Otto <git@martijnotto.nl>
2019-11-15 14:44:59 +0100

    Remove unused master_key::operator=(const std::string&)

--- master_key.h
+++ master_key.h
@@ -23,35 +23,6 @@ class master_key : public std::array<uint8_t, crypto_kdf_KEYBYTES>
         }
 
         /**
-         *  Read a password, a random salt is used
-         *
-         *  @param  password    The password for the key generation
-         */
-        master_key &operator=(const std::string &password)
-        {
-            // a buffer for the salt
-            std::array<uint8_t, crypto_pwhash_SALTBYTES> salt;
-            randombytes_buf(salt.data(), salt.size());
-
-            // generate the key from the password and salted
-            auto result = crypto_pwhash(
-                data(), size(),
-                password.data(), password.size(),
-                salt.data(),
-                crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE, crypto_pwhash_ALG_DEFAULT
-            );
-
-            // check for a valid result
-            if (result != 0) {
-                // failed to generate the password
-                throw std::runtime_error{ "Failed to generate derived key: out of memory" };
-            }
-
-            // allow chaining
-            return *this;
-        }
-
-        /**
          *  Perform symmetric (de|en)cryption on the key
          *
          *  @return The (de|en)crypted master key
